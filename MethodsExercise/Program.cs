e177bdec0cd026083715be1c725f5ce91476d5a1
Robert Zane <rob.zane24@gmail.com>
2022-11-21 10:07:02 -0600

    Completed Exercise 1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, what is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+            Console.WriteLine("What is your favorite sport?");
+            string sport = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {name}");
+            Console.WriteLine($"You are {age} years old");
+            Console.WriteLine($"Your favorite color is {color}");
+            Console.WriteLine($"Your favroite sport is {sport}");
+
+
+
+
         }
     }
 }
