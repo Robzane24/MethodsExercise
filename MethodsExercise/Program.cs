9059c5455c69a075af0b6bcc69fafd5961f93cb2
Robert Zane <rob.zane24@gmail.com>
2022-11-21 11:01:25 -0600

    Completed Exercise 2

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,8 +2,21 @@
 {
     public class Program
     {
+        public static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        
+        public static int Mulitply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
         static void Main(string[] args)
         {
+            int addTwoNum = Sum(2, 6);
+            int multiplyTwoNum = Mulitply(10, 2);
+
+
             Console.WriteLine("Hello, what is your name?");
             string name = Console.ReadLine();
             Console.WriteLine("What is your age?");
@@ -17,9 +30,6 @@
             Console.WriteLine($"You are {age} years old");
             Console.WriteLine($"Your favorite color is {color}");
             Console.WriteLine($"Your favroite sport is {sport}");
-
-
-
 
         }
     }
